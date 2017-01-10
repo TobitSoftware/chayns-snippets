761434b6036a8fec431e59cb00235fee740686aa
Benedikt Schulze Baek <benedikt.schulze-baek@tobit.software>
2017-01-10 16:20:09 +0100

    Update C#.cs

--- Backend/AddUserToUacGroup/C#.cs
+++ Backend/AddUserToUacGroup/C#.cs
@@ -1,1 +1,64 @@
+import RestSharp;
 
+public string Server = "https://api.chayns.net/v2.0";
+private const string Secret = "Your Tapp Secret";
+
+[HttpPost]
+[Route("UserPermitted")]
+public IHttpActionResult UserPermitted(int locationId, int tappId, int userId, int groupId)
+{
+    try
+    {
+        //Test if the user exists in the Group
+        dynamic user = AddUserToUacGroup(locationId, tappId, userId, groupId);
+
+        //null means add failed
+        if (user == null)
+        {
+            return InternalServerError();
+        }
+        else
+        {
+            //Build user-model
+            object userModel = new
+            {
+                userId = user.userId,
+                firstName = user.firstName,
+                lastName = user.lastName,
+                name = user.firstName,
+                gender = user.gender
+            };
+            return Ok(userModel);
+        }
+
+    }
+    catch (Exception exception)
+    {
+        return InternalServerError(exception);
+    }
+}
+
+public dynamic AddUserToUacGroup(int locationId, int tappId, int userId, int groupId)
+{
+    //Set up RestSharp
+    RestClient restClient = new RestClient(Server);
+
+    //Build the request
+    RestRequest req = new RestRequest("/" + locationId + "/Uac/" + groupId + "/User/" + userId);
+    req.Method = Method.POST;
+    req.AddHeader("Content-Type", "application/json");
+    req.AddHeader("Authorization", "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(Convert.ToString(tappId) + ':' + Secret)));
+
+    //Run the request
+    IRestResponse resp = restClient.Execute(req);
+
+    //Test response health
+    if (resp.StatusCode == HttpStatusCode.Created)
+    {
+        //Parse data
+        dynamic data = JObject.Parse(resp.Content);
+        //Just return the user-Model
+        return data.data[0];
+    }
+    return null;
+}
