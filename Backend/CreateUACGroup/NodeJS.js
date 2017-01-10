ce14a7d4ef1e251c210e7938ef7ae6902ff428df
Benedikt Schulze Baek <benedikt.schulze-baek@tobit.software>
2017-01-10 17:22:10 +0100

    Create NodeJS.js

---
+++ Backend/CreateUACGroup/NodeJS.js
@@ -0,0 +1,30 @@
+let express = require('express');
+let fetch = require('node-fetch');
+let btoa = require('btoa');
+let bodyParser = require('body-parser');
+let app = express();
+
+app.use(bodyParser.json());
+
+const Server = 'https://api.chayns.net/v2.0/';
+const Secret = 'Your Tapp Secret';
+
+app.post('/CreateUac', function(req, res) {
+    try {
+        let url = Server + req.query.locationId + '/Uac/';
+        let config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa(req.query.tappId + ':' + Secret)
+            },
+            body: JSON.stringify(req.body)
+        }
+        fetch(url, config).then(response => {
+            res.sendStatus(response.status);
+        });
+    }
+    catch(e) {
+        res.sendStatus(500);
+    }
+});
