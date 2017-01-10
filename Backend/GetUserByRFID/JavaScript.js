2c28b2e0d273cb0d62151e0a8c070efe517542ba
Benedikt Schulze Baek <benedikt.schulze-baek@tobit.software>
2017-01-10 17:10:55 +0100

    Create JavaScript.js

---
+++ Backend/GetUserByRFID/JavaScript.js
@@ -0,0 +1,23 @@
+var PageAccessToken = GetPageAccessToken(chayns.env.site.tapp.id, function (res) { return res; });
+
+var Server = 'https://api.chayns.net/v2.0/';
+function GetUserByRFID(RfidData, success, error) {
+    var config = {
+        url: Server + '/' + chayns.env.site.locationId + '/User?rfid=' + RfidData,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + PageAccessToken
+        }
+    };
+    fetch(config.url, config)
+        .then(function(response) {
+            response.json()
+                .then(function (data) {
+                    if (response.ok) {
+                        success(data.data[0]);
+                    } else {
+                        error(response);
+                    }
+                });
+        });
+}
