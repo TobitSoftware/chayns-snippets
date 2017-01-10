531b8e42f6c19c724f84b5c50052a181091fe022
Benedikt Schulze Baek <benedikt.schulze-baek@tobit.software>
2017-01-10 16:17:49 +0100

    Update JavaScript.js

--- Backend/AddUserToUacGroup/JavaScript.js
+++ Backend/AddUserToUacGroup/JavaScript.js
@@ -1,1 +1,31 @@
+var PageAccessToken = GetPageAccessToken(chayns.env.site.tapp.id, function (res) { return res; });
 
+var Server = 'https://api.chayns.net/v2.0';
+function SendMessage(message, users, groups, locations,      groupChat) {
+    var config = {
+        url: Server + '/' + chayns.env.site.locationId + '/Intercom,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + PageAccessToken
+        },
+        body: {
+            'Message': message,
+            'UacIds': groups,
+            'UserIds': users,
+            'ReceiverLocationIds': locations,
+            'UseGroupChat': groupChat
+        },
+        method: 'POST'
+    };
+    fetch(config.url, config)
+        .then(function(response) {
+            response.json()
+                .then(function (data) {
+                    if (response.status === 201) {
+                        success(data.data[0]);
+                    } else {
+                        error(response);
+                    }
+                });
+        });
+}
