af5ce54eb022bdd215114108d2579ef855fdbf77
Benedikt Schulze Baek <benedikt.schulze-baek@tobit.software>
2017-01-10 16:21:38 +0100

    Update NodeJS.js

--- Backend/AddUserToUacGroup/NodeJS.js
+++ Backend/AddUserToUacGroup/NodeJS.js
@@ -1,1 +1,26 @@
+let express = require('express');
+let fetch = require('node-fetch');
+let btoa = require('btoa');
+let app = express();
 
+const Server = 'https://api.chayns.net/v2.0/';
+const Secret = 'Your Tapp Secret';
+
+app.post('/AddUserToGroup', function(req, res) {
+    try {
+        let url = Server + req.query.locationId + '/Uac/' + req.query.groupId + '/User/' + req.query.userId;
+        let config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + btoa(req.query.tappId + ':' + Secret)
+            }
+        }
+        fetch(url, config).then(response => {
+            res.sendStatus(response.status);
+        });
+    }
+    catch(e) {
+        res.sendStatus(500);
+    }
+});
