b4bd2ff7b57794a8c61521d1f514c29a8e47df60
Benedikt Schulze Baek <benedikt.schulze-baek@tobit.software>
2017-01-10 17:00:20 +0100

    Create JavaScript.js

---
+++ Backend/IntercomToUser/JavaScript.js
@@ -0,0 +1,27 @@
+var PageAccessToken = GetPageAccessToken(chayns.env.site.tapp.id, function (res) { return res; });
+
+var Server = 'https://api.chayns.net/v2.0';
+function SendIntercom(message, users, groups, locations, groupChat) {
+    var config = {
+        url: Server + '/' + chayns.env.site.locationId + '/Intercom',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + PageAccessToken
+        },
+        body: JSON.stringify({
+            'Message': message,
+            'UserIds': users,
+            'UacIds': groups,
+            'ReceiverLocationIds': locations,
+            'UseGroupChat': groupChat
+        }),
+        method: 'POST'
+    };
+    fetch(config.url, config)
+        .then(function(response) {
+            response.json()
+                .then(function (data) {
+                    console.log(data);
+                });
+        });
+}
