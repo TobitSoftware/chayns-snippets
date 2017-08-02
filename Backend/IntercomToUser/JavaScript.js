b1be8bc2891180da9a8688c30fdd321ce406f347
Benedikt Schulze Baek <benedikt.schulze-baek@tobit.software>
2017-08-02 17:55:30 +0200

    Update JavaScript.js

--- Backend/IntercomToUser/JavaScript.js
+++ Backend/IntercomToUser/JavaScript.js
@@ -1,3 +1,5 @@
+//This snippet requires a PageAccessToken
+//To get a PageAccessToken you can use a function like set up in the 'CreatePageAccessToken' snippet
 var PageAccessToken = GetPageAccessToken(chayns.env.site.tapp.id, function (res) { return res; });
 
 var Server = 'https://api.chayns.net/v2.0';
