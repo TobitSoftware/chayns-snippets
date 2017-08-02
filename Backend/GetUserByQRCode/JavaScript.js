504ac78eb67708b669c3171eee034a04c117b454
Benedikt Schulze Baek <benedikt.schulze-baek@tobit.software>
2017-08-02 17:56:21 +0200

    Update JavaScript.js

--- Backend/GetUserByQRCode/JavaScript.js
+++ Backend/GetUserByQRCode/JavaScript.js
@@ -1,4 +1,6 @@
-var PageAccessToken = GetPageAccessToken(chayns.env.site.tapp.id, function (res) { return res; });
+//This snippet requires a PageAccessToken
+//To get a PageAccessToken you can use a function like set up in the 'CreatePageAccessToken' snippet
+var PageAccessToken = GetPageAccessToken(); 
 
 var Server = 'https://api.chayns.net/v2.0';
 function GetUserByQR(QrData, success, error) {
